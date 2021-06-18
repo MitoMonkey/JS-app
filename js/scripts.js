20c34f4b18bb1f33500f3d66f656941cf26c1df7
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-06-18 20:25:07 +0200

    display list of pokemon (with height)

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ let pokemonList = [
   {name:'Paras', height:0.3 , type:["grass", "bug"]},
   {name:'Mr. Mime', height:1.3 , type:["psychic", "fairy"]},
 ];
+
+// print a list of the pokemon (with their height) into the DOM
+for (let i=0; i<pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>');
+}
