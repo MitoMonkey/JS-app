0ef75da2c4afedf1918d7c3cac939a48dbd85fc5
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-06-18 12:59:46 +0200

    backup of first js examples

---
+++ js/scripts_first demo examples.js
@@ -0,0 +1,24 @@
+alert('Hello world');
+
+/*
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+*/
+
+let favoriteFood = 'Chocolate';
+document.write(favoriteFood);
+
+let pokemons = [{name:"picatchu", type: 'electro'}, {name:'basur', type:["water", "earth"]}];
+console.log(pokemons[1].type[0]); // will return "water"
