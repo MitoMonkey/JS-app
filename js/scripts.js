050e3b1ec1b24663902b03412d34f4bba2e1daba
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-06-18 13:00:09 +0200

    fresh scripts.js with pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,5 @@
-alert('Hello world');
-
-/*
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-*/
-
-let favoriteFood = 'Chocolate';
-document.write(favoriteFood);
-
-let pokemons = [{name:"picatchu", type: 'electro'}, {name:'basur', type:["water", "earth"]}];
-console.log(pokemons[1].type[0]); // will return "water"
+let pokemonList = [
+  {name:"Butterfree", height:1.1 , type: ["bug", "flying"]},
+  {name:'Paras', height:0.3 , type:["grass", "bug"]},
+  {name:'Mr. Mime', height:1.3 , type:["psychic", "fairy"]},
+];
