92dec449f73dc0b4cf953c9cfd3c0118bc570efd
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-06-21 14:37:59 +0200

    iife_v2

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,9 @@
 let pokemonRepository = (function () { // iife
-
   let pokemonList = [
     {name:"Butterfree", height:1.1 , type: ["bug", "flying"]},
     {name:'Paras', height:0.3 , type:["grass", "bug"]},
     {name:'Mr. Mime', height:1.3 , type:["psychic", "fairy"]},
   ];
-
-  // print a list of the pokemon (with their height) into the DOM
-  function printRepository(list) {
-    let bigComment = '';
-    list.forEach(function(pokemon) {
-      if (pokemon.height > 1.2){
-        bigComment = ' - Wow, that’s big!';
-        }
-        else {
-          bigComment = '';
-        }
-      document.write('<li>' + pokemon.name + ' (height: ' + pokemon.height + ')' + bigComment + '</li>');
-    });
-    // for (let i=0; i<list.length; i++) {
-    //   if (list[i].height > 1.2){
-    //     bigComment = ' - Wow, that’s big!';
-    //   }
-    //   else {
-    //     bigComment = '';
-    //   }
-    //   document.write('<li>' + list[i].name + ' (height: ' + list[i].height + ')' + bigComment + '</li>');
-    // }
-  }
   function getAll() {
     return pokemonList;
   }
@@ -41,10 +17,40 @@ let pokemonRepository = (function () { // iife
     getAll: getAll,
     add: add,
     remove: remove,
-    printRepo: printRepository(pokemonList)
+    // printRepo: printRepository(pokemonList)
   }
 })();
-pokemonRepository.printRepo;
+
+// print a list of the pokemon (with their height) into the DOM
+function printRepository(list) {
+  let bigComment = '';
+  list.forEach(function(pokemon) {
+    if (pokemon.height > 1.2){
+      bigComment = ' - Wow, that’s big!';
+      }
+      else {
+        bigComment = '';
+      }
+    document.write('<li>' + pokemon.name + ' (height: ' + pokemon.height + ')' + bigComment + '</li>');
+  });
+}
+printRepository(pokemonRepository.getAll());
+
+// old version of the pokemon list with a for loop:
+  // for (let i=0; i<list.length; i++) {
+  //   if (list[i].height > 1.2){
+  //     bigComment = ' - Wow, that’s big!';
+  //   }
+  //   else {
+  //     bigComment = '';
+  //   }
+  //   document.write('<li>' + list[i].name + ' (height: ' + list[i].height + ')' + bigComment + '</li>');
+  // }
+
+
+// pokemonRepository.add({name: 'testpoke', height: 4, type: "banana"});
+
+
 
 
 function divide(dividend, divisor){
