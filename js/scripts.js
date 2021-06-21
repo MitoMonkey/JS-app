536a8324b2bfcdcec2e443056552a9cfd64a1156
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-06-21 13:53:17 +0200

    iife_v1

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,41 @@
-let pokemonList = [
-  {name:"Butterfree", height:1.1 , type: ["bug", "flying"]},
-  {name:'Paras', height:0.3 , type:["grass", "bug"]},
-  {name:'Mr. Mime', height:1.3 , type:["psychic", "fairy"]},
-];
+let pokemonRepository = (function () { // iife
 
-// print a list of the pokemon (with their height) into the DOM
-function printArrayDetails(list){
-  let bigComment = '';
-  for (let i=0; i<list.length; i++) {
-    if (list[i].height > 1.2){
-      bigComment = ' - Wow, that’s big!';
+  let pokemonList = [
+    {name:"Butterfree", height:1.1 , type: ["bug", "flying"]},
+    {name:'Paras', height:0.3 , type:["grass", "bug"]},
+    {name:'Mr. Mime', height:1.3 , type:["psychic", "fairy"]},
+  ];
+
+  // print a list of the pokemon (with their height) into the DOM
+  function printRepository(list){
+    let bigComment = '';
+    for (let i=0; i<list.length; i++) {
+      if (list[i].height > 1.2){
+        bigComment = ' - Wow, that’s big!';
+      }
+      else {
+        bigComment = '';
+      }
+      document.write('<li>' + list[i].name + ' (height: ' + list[i].height + ')' + bigComment + '</li>');
     }
-    else {
-      bigComment = '';
-    }
-    document.write('<li>' + list[i].name + ' (height: ' + list[i].height + ')' + bigComment + '</li>');
   }
-}
-printArrayDetails(pokemonList);
+  function getAll {
+    return pokemonList;
+  }
+  function add(newPokemon) {
+    pokemonList.push(newPokemon); // validation of data format required
+  }
+  function remove(index){
+  //  pokemonList.push(newPokemon); // not implemented yet (may name is better than index > indexOf() )
+  }
+  return {
+    getAll: getAll,
+    add: add,
+    remove: remove,
+    printRepo: printRepository(pokemonList)
+  }
+})();
+
 
 function divide(dividend, divisor){
   if (divisor === 0) {return "You are trying to devide by zero!";}
