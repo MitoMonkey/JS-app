1b6b7679879e8400ca683af5a538cec746f02988
MitoMonkey <85686133+MitoMonkey@users.noreply.github.com>
2021-07-05 18:41:06 +0200

    added modal

--- js/scripts.js
+++ js/scripts.js
@@ -79,6 +79,59 @@ function add(pokemon) {
     });
   }
 
+  let modalContainer = document.querySelector('#modal-container');
+
+  function showModal(title, text) {
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
+
+  // hide the modal when user presses ESC key
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    // This is also triggered when clicking INSIDE the modal
+    // But we only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
   return {
     getAll: getAll,
     add: add,
@@ -89,7 +142,7 @@ function add(pokemon) {
     // filter: findPokemon,
     // printRepo: printRepository(pokemonList)
   }
-})();
+})(); // End if IIFE
 
 // print a list of the pokemon into the DOM
 pokemonRepository.loadList().then(function() {
